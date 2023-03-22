08ceb8f63614f03563ab474748fd8ec3b0a43e38
doremonRudraksh <90757183+doremonRudraksh@users.noreply.github.com>
2023-03-22 14:07:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot() {
 
 console.log("ml5 version:",ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Bly-O0woQ//modelLoaded');
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_R2QprhuK//modelLoaded');
 
 function modelLoaded() {
     console.log("Model Loaded Successfully!");
